40275624aa9a7fd5aaa6bd8849ecd84c7520664a
BHObserver <BHObserver0@gmail.com>
2023-09-11 16:19:58 +0600

    Delete equation1.js

--- src/equations/category1/basic-time-study/equation1.js
+++
@@ -1,6 +0,0 @@
-export function calculateEquation1(observedTime, allowances) {
-    // Calculate standard time (ST) as the sum of observed time and allowances
-    const standardTime = observedTime + allowances;
-
-    return standardTime;
-}
