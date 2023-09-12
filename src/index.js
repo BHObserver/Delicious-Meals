ddbe9a3300c90dbc1e40e262def6787a7b87f367
Umu <hanny.ally123@gmail.com>
2023-09-12 20:40:12 +0300

    Add index.js src file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+import fetchMeals from './modules/fetchMeals.js';
+import displayMeals from './modules/displayMeals.js';
+
+const start = async () => {
+  const data = await fetchMeals();
+  displayMeals(data);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  start();
+  JSON.parse(localStorage.getItem('likes'));
+});
