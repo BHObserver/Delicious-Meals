95db78f8834b468b0d1efeb22e34f74b86ce8ac1
Umu <hanny.ally123@gmail.com>
2023-09-12 20:41:41 +0300

    Add displayMeals.js module file

---
+++ src/modules/displayMeals.js
@@ -0,0 +1,44 @@
+const productsDOM = document.querySelector('.products-center');
+
+// display function
+const displayMeals = (list) => {
+  const productList = list.meals
+    .map((item) => {
+      const { idMeal } = item;
+      // console.log(idMeal);
+      const { strMeal: title } = item;
+      const { strMealThumb: img } = item;
+
+      return ` <a class="single-product" href="#">
+      <img
+        src="${img}"
+        alt="${title}"
+        class="single-product-img img"
+      />
+      <footer>
+        <h5 class="name">${title}</h5>
+        <div class="btn-container">
+        <button class="btn-like" type="button" >&#10084 <span class="btn-span">0 likes</span></button>
+        <button class="btn-comment">COMMENTS</button>
+        </div>
+      </footer>
+    </a>`;
+    })
+    .join('');
+  productsDOM.innerHTML = `<div class="products-container">
+  ${productList}
+  </div>`;
+  const likeBtns = [...document.querySelectorAll('.btn-like')];
+  likeBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const span = btn.querySelector('.btn-span');
+      let count = 0;
+      count += span.textContent;
+      const newCount = parseInt(count) + 1;
+      span.textContent = `${newCount} likes`;
+      localStorage.setItem('likes', JSON.stringify(newCount));
+    });
+  });
+};
+
+export default displayMeals;
