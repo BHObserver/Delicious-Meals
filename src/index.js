4559306c68671929a2e0232d3e6e87940def9f35
BHObserver <BHObserver0@gmail.com>
2023-09-14 01:03:25 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './style.css';
+import cancelIcon from './assets/cancel-icon.png';
 import fetchMeals from './modules/fetchMeals.js';
 import displayMeals from './modules/displayMeals.js';
+import show, { cancel } from './modules/commentPopup';
+
+const cancelIconContainer = document.querySelector('.close-pop-up');
+cancelIconContainer.setAttribute('src', cancelIcon);
 
 const start = async () => {
   const data = await fetchMeals();
@@ -10,4 +15,9 @@ const start = async () => {
 document.addEventListener('DOMContentLoaded', () => {
   start();
   JSON.parse(localStorage.getItem('likes'));
+});
+
+document.addEventListener('click', () => {
+  show();
+  cancel();
 });
