bf29c020e3be43ab8e55856b62435f37bc4804ae
BHObserver <BHObserver0@gmail.com>
2023-09-14 01:03:27 +0600

    Update commentPopup.js

--- src/modules/commentPopup.js
+++ src/modules/commentPopup.js
@@ -1,1 +1,33 @@
 const allProducts = document.querySelectorAll('.btn-comment');
+const popUp = document.querySelector('.blur-bg');
+const popUpImg = popUp.querySelector('.img-pop-up');
+const popUpHeading = popUp.querySelector('.pop-up-heading');
+
+export default function show() {
+  document.addEventListener('click', (event) => {
+    const clickedElement = event.target;
+    if (clickedElement.className === 'btn-comment') {
+      const parentElement = clickedElement.closest('section');
+      const img = parentElement.children[0].getAttribute('src');
+      const title = parentElement.children[1].getElementsByTagName('h5')[0].innerText;
+      popUpImg.setAttribute('src', img);
+      popUpHeading.innerHTML = `<h2>${title}</h2>`;
+      popUp.style.display = 'block';
+    }
+  });
+}
+
+export function cancel() {
+  document.addEventListener('click', (event) => {
+    const clickedElement = event.target;
+    if (clickedElement.className === 'close-pop-up') {
+      popUp.style.display = 'none';
+    }
+  });
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      popUp.style.display = 'none';
+    }
+  });
+}
