3f280eb6e6b4b90ac911915c865ff1adba206167
Umu <hanny.ally123@gmail.com>
2023-09-13 17:13:29 +0300

    Modify files

--- src/modules/fetchMeals.js
+++ src/modules/fetchMeals.js
@@ -1,18 +1,17 @@
 // fetch  function
-const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian';
+const url = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian";
 
-const productsDOM = document.querySelector('.products-center');
+const productsDOM = document.querySelector(".products-center");
 
 const fetchMeals = async () => {
-  productsDOM.innerHTML = 'Loading...';
+  productsDOM.innerHTML = "Loading...";
   try {
     const response = await fetch(url);
     const data = await response.json();
     return data;
   } catch (error) {
-    productsDOM.innerHTML = 'Error detected';
+    productsDOM.innerHTML = "Error detected";
   }
-  return null;
 };
 
 export default fetchMeals;
