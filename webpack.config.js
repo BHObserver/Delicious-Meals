7604a2d8c942f70d2a83712ecb9d61ea16b5e140
BHObserver <BHObserver0@gmail.com>
2023-09-14 22:30:21 +0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,11 +25,22 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: ['file-loader'],
       },
     ],
   },
