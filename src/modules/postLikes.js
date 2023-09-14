8a518f02ad3f12a719b8855d9ef47b774320ef40
BHObserver <BHObserver0@gmail.com>
2023-09-15 05:01:47 +0600

    Update postLikes.js

--- src/modules/postLikes.js
+++ src/modules/postLikes.js
@@ -1,10 +1,10 @@
-import baseURL from "./baseURL.js";
+import baseURL from './baseURL.js';
 
 const postLikes = async (id) => {
   const response = await fetch(`${baseURL}/likes`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ item_id: id }),
   });
