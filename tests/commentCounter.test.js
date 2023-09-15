52964ba8e9184d7ba1bc6c6b0258d915d5e0503f
BHObserver <BHObserver0@gmail.com>
2023-09-15 23:52:19 +0600

    Update commentCounter.test.js

--- tests/commentCounter.test.js
+++ tests/commentCounter.test.js
@@ -2,10 +2,10 @@
  * @jest-environment jsdom
  */
 
-const addCommentCounter = require("./_mock/commentCounter.js");
+const addCommentCounter = require('./_mock/commentCounter.js');
 
-describe("addCommentCounter", () => {
-  test("Should return the number of comments elements in the UI", () => {
+describe('addCommentCounter', () => {
+  test('Should return the number of comments elements in the UI', () => {
     document.body.innerHTML = `<p class='comment_paragraph'</p>   
             <p class='comment_paragraph'</p> 
             <p class='comment_paragraph'</p>  
@@ -14,7 +14,7 @@ describe("addCommentCounter", () => {
     expect(addCommentCounter()).toBe(5);
   });
 
-  test("Should return the number of comments elements in the UI", () => {
+  test('Should return the number of comments elements in the UI', () => {
     document.body.innerHTML = "<p class='comment_paragraph'</p>";
     expect(addCommentCounter()).toBe(1);
   });
