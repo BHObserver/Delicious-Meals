a37ab85d7ae655b0fc097588e202e0e3e90159c4
BHObserver <BHObserver0@gmail.com>
2023-09-15 23:43:53 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import "./style.css";
-import fetchMeals from "./modules/fetchMeals.js";
-import displayMeals from "./modules/displayMeals.js";
-import getLikes from "./modules/getLikes.js";
-import itemsCounter from "./modules/itemCount.js";
+import './style.css';
+import fetchMeals from './modules/fetchMeals.js';
+import displayMeals from './modules/displayMeals.js';
+import getLikes from './modules/getLikes.js';
+import itemsCounter from './modules/itemCount.js';
 
 const start = async () => {
   const data = await fetchMeals();
   displayMeals(data);
 };
 
-window.addEventListener("DOMContentLoaded", async () => {
+window.addEventListener('DOMContentLoaded', async () => {
   getLikes();
   await start();
   itemsCounter();
