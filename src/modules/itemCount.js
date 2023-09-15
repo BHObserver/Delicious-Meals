e5f46544b16fa8883de0619c9a6328577c32e5f6
BHObserver <BHObserver0@gmail.com>
2023-09-16 02:31:46 +0600

    Update itemCount.js

--- src/modules/itemCount.js
+++ src/modules/itemCount.js
@@ -1,6 +1,5 @@
 const itemsCounter = () => {
   const lists = document.querySelectorAll('.single-product');
-  console.log(lists)
   const displayNumber = lists.length;
   const NumberOfCounts = document.querySelector('#count');
   NumberOfCounts.textContent = displayNumber;
