6ffbcd9ef98605f232519dabdbe3f05eba9928c4
BHObserver <BHObserver0@gmail.com>
2023-09-16 00:40:25 +0600

    Update commentCounter.js

--- src/modules/commentCounter.js
+++ src/modules/commentCounter.js
@@ -1,11 +1,3 @@
-/* const fetchCommentsCount = async (itemId) => {
-  const apiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Y9oi34w1aSiFtzn6asAJ/comments?item_id=';
-  const response = await fetch(`${apiUrl}${itemId}`);
-  const comments = await response.json();
-  return comments.length;
-};
-
-export default fetchCommentsCount; */
 const commentCount = () => {
   const commentList = document.querySelector('#commentsList');
   const inputContainer = document.querySelector('.input-container');
