07821533839e947e3f1ee2e1926863cdb3630e29
BHObserver <BHObserver0@gmail.com>
2023-09-15 23:16:56 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './style.css';
 import cancelIcon from './assets/cancel-icon.png';
 import fetchMeals from './modules/fetchMeals.js';
 import displayMeals from './modules/displayMeals.js';
+/* import fetchCommentsCount from './modules/commentCounter.js'; */
+import commentCount from './modules/commentCounter.js';
 import show, { cancel } from './modules/commentPopup.js';
 import { submitComment } from './modules/comment.js';
 
@@ -10,6 +12,21 @@ cancelIconContainer.setAttribute('src', cancelIcon);
 const start = async () => {
   const data = await fetchMeals();
   displayMeals(data);
+  /* const itemId = [];
+  const { meals } = data;
+  meals.forEach((element) => {
+    itemId.push(parseInt(element.idMeal, 10));
+  });
+
+  itemId.forEach((element) => {
+    console.log(fetchCommentsCount(element));
+  }); */
+  /* const commentCounterElement = document.querySelectorAll('.comment-counter');
+  commentCounterElement.forEach((element) => {
+    itemId.forEach((item) => {
+      element.textContent =
+    })
+  }) */
 };
 
 document.addEventListener('DOMContentLoaded', () => {
