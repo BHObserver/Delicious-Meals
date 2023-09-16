9f0a29f16dbef7ab9fd84e2a119184283b77a41c
BHObserver <BHObserver0@gmail.com>
2023-09-16 13:42:39 +0600

    Update getLikes.js

--- src/modules/getLikes.js
+++ src/modules/getLikes.js
@@ -1,9 +1,33 @@
-import baseURL from './baseURL.js';
-
-const getLikes = async () => {
+const allLikes = async () => {
+  const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/0TWgjluCPP5AOa5XVRrg';
   const response = await fetch(`${baseURL}/likes/`);
-  const likes = await response.json();
-  return likes;
+  const data = await response.json();
+  return data;
 };
 
-export default getLikes;
+const getLikes = async () => {
+  const likes = await allLikes();
+  return likes; // Just return the array
+};
+
+// To use the getLikes function and store the array in a variable:
+const fetchLikes = async () => {
+  const likesUpdate = document.querySelectorAll('.btn-span');
+  try {
+    const response = await getLikes();
+    const likesArray = [];
+    response.forEach((item) => {
+      likesArray.push(item.likes);
+    });
+
+    likesUpdate.forEach((element) => {
+      element.textContent = likesArray.shift();
+    });
+  } catch (error) {
+    // Handle any errors that occurred during the fetch
+    console.error('Error fetching likes:', error);
+  }
+};
+
+// Call the fetchDataAndStore function to retrieve and store the array
+export default fetchLikes;
