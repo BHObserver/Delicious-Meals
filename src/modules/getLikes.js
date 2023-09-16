49084870550fba853d642ea3c58c277ee1de9394
BHObserver <BHObserver0@gmail.com>
2023-09-16 19:01:46 +0600

    Update getLikes.js

--- src/modules/getLikes.js
+++ src/modules/getLikes.js
@@ -1,19 +1,10 @@
-const allLikes = async () => {
+const fetchLikes = async () => {
   const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/0TWgjluCPP5AOa5XVRrg';
   const response = await fetch(`${baseURL}/likes/`);
   const data = await response.json();
-  return data;
-};
-
-const getLikes = async () => {
-  const likes = await allLikes();
-  return likes;
-};
-
-const fetchLikes = async () => {
   const likesUpdate = document.querySelectorAll('.btn-span');
   try {
-    const response = await getLikes();
+    const response = data;
     const likesArray = [];
     response.forEach((item) => {
       likesArray.push(item.likes);
